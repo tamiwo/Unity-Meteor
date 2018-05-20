7fdc3b6c17ad98fc989f61514a096803af2088fe
Tamiwo <taimwo.otutama@gmail.com>
2018-05-20 13:32:21 +0900

    Initial commit

---
+++ TempleUnityProject/Assets/Scripts/GameManager.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour {
+
+    //定数定義
+    private const int MAX_ORB = 10;
+
+    //オブジェクト参照
+    public GameObject orbPrefab;
+    public GameObject canvasGame;
+    public GameObject textScore;
+
+    //メンバ変数
+    private int score = 0;
+    private int nextScore = 100;
+
+	// Use this for initialization
+	void Start () {
+        //初期オーブ生成
+        for (int i = 0; i < MAX_ORB; i++){
+            CreateOrb();
+        }
+        RefreshScoreText();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    // オーブ生成
+    public void CreateOrb () {
+        GameObject orb = (GameObject)Instantiate(orbPrefab);
+        orb.transform.SetParent(canvasGame.transform, false);
+        orb.transform.localPosition = new Vector3(
+            UnityEngine.Random.Range(-300.0f, 300.0f),
+            UnityEngine.Random.Range(-140.0f, -500.0f),
+            0f);
+    }
+
+    //オーブ入手
+    public void GetOrb (){
+        score += 1;
+        RefreshScoreText();
+    }
+
+    //スコアてきすと更新
+    void RefreshScoreText (){
+        textScore.GetComponent<Text>().text =
+            "Score：" + score + "/" + nextScore;
+    }
+}
---
+++ TempleUnityProject/Assets/Scripts/OrbManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrbManager : MonoBehaviour {
+
+    //オブジェクト参照
+    private GameObject gameManager;
+
+	// Use this for initialization
+	void Start () {
+        gameManager = GameObject.Find("GameManager");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    //オーブ取得
+    public void TouchOrb () {
+        Debug.Log("touch the orb.");
+        if (Input.GetMouseButton (0) == false ){
+            return;
+        }
+
+        gameManager.GetComponent<GameManager>().GetOrb();
+        Destroy(this.gameObject);
+    } 
+}
