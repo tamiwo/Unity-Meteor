7466833bb21f95093687d0a392c9a5e99f9bb29e
kotthan <otutama.250ml@gmail.com>
2018-05-20 18:26:14 +0900

    update:OutZoneManagerを追加
    ゲームオーバーのコードをこちらに移動させたが、うまく動かない

---
+++ TempleUnityProject/Assets/Scripts/OutZoneManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OutZoneManager : MonoBehaviour {
+	public GameObject gameManager;				//ゲームマネージャー
+	public GameObject player;					//プレイヤー
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+	//衝突処理
+	void OnTriggerEnter2D(Collider2D col){
+		if (col.gameObject.tag == "Trap") {
+			gameManager.GetComponent<GameManager> ().GameOver ();
+			DestroyPlayer ();
+		}
+	}
+
+	//プレイヤーオブジェクト削除処理
+	void DestroyPlayer(){
+		Destroy (player.gameObject);
+	}
+}
