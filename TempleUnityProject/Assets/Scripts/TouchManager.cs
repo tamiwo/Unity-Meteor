46b6d539dbb241745ba6559730747054baba87ae
Tamiwo <taimwo.otutama@gmail.com>
2018-05-31 07:58:47 +0900

    update:スワイプ判定

--- TempleUnityProject/Assets/Scripts/TouchManager.cs
+++ TempleUnityProject/Assets/Scripts/TouchManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 using TouchScript.Gestures;
+using TouchScript.Gestures.TransformGestures;
 using TouchScript.Hit;
 
 public class TouchManager : MonoBehaviour {
@@ -19,15 +20,136 @@ public class TouchManager : MonoBehaviour {
     [SerializeField]
     private UnityEvent tap;
 
+    // 設定値
+    public static float minimumDistance = 30.0f;   //最小スワイプ判定距離
+
+
+    private Vector2 _touchStartPoint;   //タッチ開始位置
+    enum Direction { UP, DOWN, RIGHT, LEFT };
+
     private void OnEnable()
     {
-        GetComponent<FlickGesture>().Flicked += OnFlick;
+        // フリック
+        //GetComponent<FlickGesture>().Flicked += OnFlick;
+        // タップ
         GetComponent<TapGesture>().Tapped += OnTap;
+        // 移動
+        GetComponent<ScreenTransformGesture>().Transformed += OnTransform;
+        GetComponent<ScreenTransformGesture>().TransformStarted += OnTransformStart;
+        GetComponent<ScreenTransformGesture>().TransformCompleted += OnTransformComplete;
     }
+
     private void OnDisable()
     {
-        GetComponent<FlickGesture>().Flicked -= OnFlick;
+        // フリック
+        //GetComponent<FlickGesture>().Flicked -= OnFlick;
+        // タップ
         GetComponent<TapGesture>().Tapped -= OnTap;
+        // 移動
+        GetComponent<ScreenTransformGesture>().Transformed -= OnTransform;
+        GetComponent<ScreenTransformGesture>().TransformStarted -= OnTransformStart;
+        GetComponent<ScreenTransformGesture>().TransformCompleted -= OnTransformComplete;
+    }
+
+    // Transform開始時に呼ばれる
+    private void OnTransformStart(object sender, System.EventArgs e){
+        Debug.Log("Transform Start.");
+        //タッチ位置を記録する
+        ScreenTransformGesture gesture = (ScreenTransformGesture)sender;
+        _touchStartPoint = gesture.ScreenPosition;
+    }
+
+    // Transform中に呼ばれる
+    private void OnTransform(object sender, System.EventArgs e)
+    {
+        //タッチ位置を取得する
+        ScreenTransformGesture gesture = (ScreenTransformGesture)sender;
+        var touchPoint = gesture.ScreenPosition;
+        //開始位置からの移動量をだす
+        var moveVector = touchPoint - _touchStartPoint;
+
+        // 移動量が一定以上でなければスワイプ判定せずに抜ける
+        if ( ( moveVector.x < minimumDistance ) && 
+             ( moveVector.y < minimumDistance ) ){
+            return;
+        }
+
+        // スワイプ方向判定
+        Direction dir = Vec2Dirction(moveVector);
+        Debug.Log("Swipe " + moveVector + " (" + dir + ")");
+        switch( dir ){
+            case Direction.UP:
+                break;
+            case Direction.DOWN:
+                break;
+            case Direction.RIGHT:
+                break;
+            case Direction.LEFT:
+                break;
+        }
+    }
+
+    // Transfrom終了時に呼ばれる
+    private void OnTransformComplete(object sender, System.EventArgs e)
+    {
+        ScreenTransformGesture gesture = (ScreenTransformGesture)sender;
+        var touchPoint = gesture.ScreenPosition;
+        //開始位置からの移動量をだす
+        var moveVector = touchPoint - _touchStartPoint;
+
+        // 移動量が一定以上でなければスワイプ判定せずに抜ける
+        if ((moveVector.x < minimumDistance) &&
+             (moveVector.y < minimumDistance))
+        {
+            
+            return;
+        }
+
+        // スワイプ方向判定
+        Direction dir = Vec2Dirction(moveVector);
+        Debug.Log("Swipe " + dir + " End.");
+        switch (dir)
+        {
+            case Direction.UP:
+                swipeUp.Invoke();
+                break;
+            case Direction.DOWN:
+                swipeDown.Invoke();
+                break;
+            case Direction.RIGHT:
+                swipeRight.Invoke();
+                break;
+            case Direction.LEFT:
+                swipeLeft.Invoke();
+                break;
+        }
+    }
+
+    //スワイプ方向判定
+    private Direction Vec2Dirction ( Vector2 vector ){
+
+        if (Mathf.Abs(vector.y) >= Mathf.Abs(vector.x)) //縦方向
+        {
+            if (vector.y >= 0) //上
+            {
+                return Direction.UP;
+            }
+            else //下
+            {
+                return Direction.DOWN;
+            }
+        }
+        else //横方向
+        {
+            if (vector.x >= 0) //右
+            {
+                return Direction.RIGHT;
+            }
+            else //左
+            {
+                return Direction.LEFT;
+            }
+        }
     }
 
     // タップ時に呼ばれるメソッド
@@ -72,5 +194,4 @@ public class TouchManager : MonoBehaviour {
             }
         }
     }
-
 }
