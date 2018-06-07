248f12cf4ccbfac4b4e49cc53326e764969e566d
tamiwo <tamiwo.otutama@gmail.com>
2018-06-07 20:05:04 +0900

    update:GuardShapeManager追加(仮)
    ジャンプしたプレイヤーと衝突してばらける

---
+++ TempleUnityProject/Assets/Scripts/GuardShapeManager.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GuardShapeManager : MonoBehaviour
+{
+    
+    public float max = 20.0f;           //最大値
+    public float dafault = 20.0f;       //初期値
+    public float lossRate = 1.0f;       //ガード中に減少量[/s]
+    public float gainRate = 1.0f;       //回復中の増加量[/s]
+    public float lostByMeteor = 7.0f;   //隕石と接触したときに減らす量
+    private float power = 0f;
+
+    // Use this for initialization
+    void Start()
+    {
+        power = dafault;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var guardShape = this.gameObject;
+
+        Debug.Log("GaurdShape pow:" + power );
+
+        if (guardShape.activeSelf == true)// ガード中
+        {
+            power -= Time.deltaTime * lossRate;
+            if (power < 0)
+            {
+                this.gameObject.SetActive(false);
+                Debug.Log("GaurdShape Inactivate.");
+            }
+        }
+        else { // ガードしてない
+            if (power < max) //回復中
+            {
+                power -= Time.deltaTime * gainRate;
+                this.gameObject.SetActive(false);
+                Debug.Log("GaurdShape Inactivate.");
+            }
+        }            
+    }
+
+    public void GuardShapeActive()
+    {
+        Debug.Log("GuardShape Activate.");
+        this.gameObject.SetActive(true);
+    }
+
+    //衝突処理
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        var obj = col.gameObject;
+        if (obj.tag == "Meteor")
+        {
+            //GuardShape減少
+            power -= lostByMeteor;
+            this.gameObject.SetActive(false);
+        }
+    }
+}
