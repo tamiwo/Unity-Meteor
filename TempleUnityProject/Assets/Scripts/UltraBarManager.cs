e6a1ef756e0ae84fd4089583644ce06088f96ba8
Tamiwo <taimwo.otutama@gmail.com>
2018-07-08 16:50:31 +0900

    グレースケール用

--- TempleUnityProject/Assets/Scripts/UltraBarManager.cs
+++ TempleUnityProject/Assets/Scripts/UltraBarManager.cs
@@ -1,19 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UltraBarManager : MonoBehaviour {
 
-    public GameObject barMask; 
+    public GameObject barMask;
+    public GameObject barPush;
+    public GameObject buttonUltra;
+    public GameObject buttonUltraBack;
+    public Material btnDisable;
+    public Material btnEnable;
 
     private Vector3 barScaleOrigin;
     private Vector3 barScale;
 
 	// Use this for initialization
 	void Start () {
+        //初期値(100%)の長さを取得する
         barScale = barMask.transform.localScale;
         barScaleOrigin = new Vector3(barScale.x,barScale.y);
-        SetScale(0.5f);
+
+        SetScale(0.99f);
+        disableUltra();
+
 	}
 	
 	// Update is called once per frame
@@ -21,7 +31,34 @@ public class UltraBarManager : MonoBehaviour {
 
 	}
 
-    public void SetScale( float scale ){
+    public void SetScale (float scale) {
+
+        // 0から1の範囲に収める
+        if( scale > 1.0f ){
+            scale = 1.0f;
+        }
+        else if ( scale < 0.0f ){
+            scale = 0.0f;
+        }
+
+        if( scale >= 1.0f ){
+            enableUltra();
+        }
+        else{
+            //100%以外はアイコンの横まで伸縮させる
+            scale = scale * (2.75f / 4);
+        }
         barMask.transform.localScale = new Vector3(barScaleOrigin.x * scale, barScaleOrigin.y);
+
+    }
+     
+    void enableUltra(){
+        barPush.SetActive(true);
+
+    }
+    void disableUltra(){
+        barPush.SetActive(false);
+        buttonUltra.GetComponent<Button>().interactable = false;
+        buttonUltra.GetComponent<Renderer>().material = btnDisable;
     }
 }
