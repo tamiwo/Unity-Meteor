871366beb28d3b51590fa59966383a40bed9816c
Tamiwo <taimwo.otutama@gmail.com>
2018-08-14 15:58:34 +0900

    update:一時停止ボタン

---
+++ TempleUnityProject/Assets/Scripts/PauseButtonManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseButtonManager : MonoBehaviour {
+
+    public GameObject pauseButton;
+    public GameObject restartButton;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void Pause(){
+        Time.timeScale = 0.0f;
+        pauseButton.SetActive(false);
+        restartButton.SetActive(true);
+    }
+
+    public void Restart(){
+        Time.timeScale = 1.0f;
+        pauseButton.SetActive(true);
+        restartButton.SetActive(false);
+    }
+
+}
