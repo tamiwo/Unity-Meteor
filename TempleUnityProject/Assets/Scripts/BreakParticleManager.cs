10337e40b6cf78b146ef39860c18773d1ef50bdd
Tamiwo <taimwo.otutama@gmail.com>
2018-08-14 14:11:55 +0900

    update:隕石破壊時のパーティクル追加

---
+++ TempleUnityProject/Assets/Scripts/BreakParticleManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BreakParticleManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Destroy(this.gameObject, 5.0f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
