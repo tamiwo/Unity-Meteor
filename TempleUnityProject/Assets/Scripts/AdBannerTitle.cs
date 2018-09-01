2e5935f02d57fef5dc0c26a75fbc756d290da381
Tamiwo <taimwo.otutama@gmail.com>
2018-09-02 00:37:03 +0900

    add:タイトル画面に広告追加

---
+++ TempleUnityProject/Assets/Scripts/AdBannerTitle.cs
@@ -0,0 +1,107 @@
+﻿using UnityEngine;
+using System;
+using GoogleMobileAds.Api;
+
+public class AdBannerTitle : MonoBehaviour
+{
+    private BannerView banner;
+
+    public void OnEnable()
+    {
+
+#if UNITY_ANDROID
+        string appId = "ca-app-pub-3940256099942544~3347511713";
+#elif UNITY_IPHONE
+        string appId = "ca-app-pub-3940256099942544~1458002511";
+#else
+        string appId = "unexpected_platform";
+#endif
+
+        MobileAds.SetiOSAppPauseOnBackground(true);
+
+        // Initialize the Google Mobile Ads SDK.
+        MobileAds.Initialize(appId);
+
+        Request();
+    }
+
+
+    public void OnDisable()
+    {
+        banner.Destroy();
+    }
+
+    public void Request()
+    {
+        // These ad units are configured to always serve test ads.
+#if UNITY_EDITOR
+        string adUnitId = "unused";
+#elif UNITY_ANDROID
+        string adUnitId = "ca-app-pub-3940256099942544/6300978111";
+#elif UNITY_IPHONE
+        string adUnitId = "ca-app-pub-3940256099942544/2934735716";
+#else
+        string adUnitId = "unexpected_platform";
+#endif
+
+        // Clean up banner ad before creating a new one.
+        if (banner != null)
+        {
+            banner.Destroy();
+        }
+
+        // Create a 300 x 250 banner at the top of the screen.
+        banner = new BannerView(adUnitId, AdSize.Banner, AdPosition.Bottom);
+
+        // Register for ad events.
+        banner.OnAdLoaded += HandleAdLoaded;
+        banner.OnAdFailedToLoad += HandleAdFailedToLoad;
+        banner.OnAdOpening += HandleAdOpened;
+        banner.OnAdClosed += HandleAdClosed;
+        banner.OnAdLeavingApplication += HandleAdLeftApplication;
+
+        // Load a banner ad.
+        banner.LoadAd(CreateAdRequest());
+    }
+
+    // Returns an ad request with custom ad targeting.
+    private AdRequest CreateAdRequest()
+    {
+        return new AdRequest.Builder()
+            .AddTestDevice(AdRequest.TestDeviceSimulator)
+            .AddTestDevice("0123456789ABCDEF0123456789ABCDEF")
+            .AddKeyword("game")
+            .SetGender(Gender.Male)
+            .SetBirthday(new DateTime(1985, 1, 1))
+            .TagForChildDirectedTreatment(false)
+            .AddExtra("color_bg", "9B30FF")
+            .Build();
+    }
+  
+    #region Banner callback handlers
+    public void HandleAdLoaded(object sender, EventArgs args)
+    {
+        MonoBehaviour.print("HandleAdLoaded event received");
+    }
+
+    public void HandleAdFailedToLoad(object sender, AdFailedToLoadEventArgs args)
+    {
+        MonoBehaviour.print("HandleFailedToReceiveAd event received with message: " + args.Message);
+    }
+
+    public void HandleAdOpened(object sender, EventArgs args)
+    {
+        MonoBehaviour.print("HandleAdOpened event received");
+    }
+
+    public void HandleAdClosed(object sender, EventArgs args)
+    {
+        MonoBehaviour.print("HandleAdClosed event received");
+    }
+
+    public void HandleAdLeftApplication(object sender, EventArgs args)
+    {
+        MonoBehaviour.print("HandleAdLeftApplication event received");
+    }
+    #endregion
+}
