ff873e51b3e06c15e7d8664047398f47773bed6a
Tamiwo <taimwo.otutama@gmail.com>
2018-09-15 18:44:02 +0900

    update:Push!の点滅

---
+++ TempleUnityProject/Assets/Scripts/PushManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PushManager : MonoBehaviour {
+
+    Color color;
+    public float flashSpeed = 3;
+
+	void Start () {
+        GetComponent<SpriteRenderer>().color = Color.black;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+        float alpha = (Mathf.Sin(Time.time* flashSpeed) + 1) / 2;
+        Color newColor = new Color(1, 1, 1, alpha);
+        GetComponent<SpriteRenderer>().color = newColor;
+	}
+}
