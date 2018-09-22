f631e363888fe4631ddcf029062fe548d0f981ca
Tamiwo <taimwo.otutama@gmail.com>
2018-09-22 16:03:06 +0900

    add:チュートリアルシーン
    GameSceneのコピー
    GameManegerのスクリプトはそのまま
    Tutorialの文字を追加
    TutorialManagerを追加
    GameObjectが出した隕石を削除する

---
+++ TempleUnityProject/Assets/Scripts/Tutorial/TutorialManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class TutorialManager : MonoBehaviour {
+
+    public GameObject gameManagerObj;
+    private GameManager gameManager;
+
+    enum STATE{
+        START,
+        ATTACK,
+        JUMP,
+        GUARD,
+        END
+    }
+    STATE status = STATE.START;
+
+	// Use this for initialization
+	void Start () {
+        gameManager = gameManagerObj.GetComponent<GameManager>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+        switch(status){
+            case STATE.START:
+                //隕石が出されていたら消す
+                GameObject meteor = GameObject.FindGameObjectWithTag("Meteor");
+                if( meteor != null ){
+                    Destroy(meteor);
+                    status = STATE.ATTACK;
+                    Debug.Log("Destory Meteor");
+                }
+                break;
+            default:
+                break;
+        }
+
+	}
+}
